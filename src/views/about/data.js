ee13afb9382a7c72617dcb8502248fe48930c836
Andrés Rojas <aerojasc@gmail.com>
2022-08-23 00:14:25 -0400

    adición de textos para vista de quienes somos

--- src/views/about/data.js
+++ src/views/about/data.js
@@ -1,19 +1,19 @@
 export const blocks = [
   {
-    bg: require('@/assets/images/bg-6.jpg'),
-    icon: require('@/assets/images/icon-3.svg'),
-    text: 'Lorem ipsum dolor sit amet consectetuz.'
-  }, {
     bg: require('@/assets/images/bg-7.jpg'),
+    icon: require('@/assets/images/icon-3.svg'),
+    text: 'Misión'
+  }, {
+    bg: require('@/assets/images/bg-6.jpg'),
     icon: require('@/assets/images/icon-4.svg'),
-    text: 'Lorem ipsum veritatis dolorum id voluptate.'
+    text: 'Visión'
   }
 ]
 
 export const strategies = [
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum saepe iure voluptatem impedit velit eveniet maiores sit, illum ad ut! Voluptate doloremque veritatis.',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum saepe iure voluptatem impedit velit eveniet maiores sit, illum ad ut! Voluptate doloremque veritatis.',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum saepe iure voluptatem impedit velit eveniet maiores sit, illum ad ut! Voluptate doloremque veritatis.',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum saepe iure voluptatem impedit velit eveniet maiores sit, illum ad ut! Voluptate doloremque veritatis.',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum saepe iure voluptatem impedit velit eveniet maiores sit, illum ad ut! Voluptate doloremque veritatis.'
+  'Ofrecer los más competitivos servicios de Aduana, así como asesorías técnicas, legales y tributarias.',
+  'Utilizar las tecnologías más avanzadas para estar siempre conectado con el cliente, en el lugar y al tiempo apropiado.',
+  'Manejo logístico del transporte interno, así como el seguimiento de la carga marítima y aérea.',
+  'Facilitar a la empresa el acceso a un entrenamiento y capacitación del personal responsable en el área.',
+  'Disposición de un servicio integral para las operaciones aduaneras. (Importación, Exportación y Tránsito).'
 ]
