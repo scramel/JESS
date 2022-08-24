4d7af353d1549efe1dd15193822081de7c79cc68
Andrés Rojas <aerojasc@gmail.com>
2022-08-24 12:41:27 -0400

    correcciones estilos en vista de home

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-5 bg-primary text-white">
+  <div class="py-5 bg-secondary  text-white">
     <b-container>
       <b-row>
         <b-col lg="3">
@@ -8,7 +8,7 @@
           </div>
           <p>
             <small>
-              Todos los derechos reservados 2022. Corporacionjess.com
+              Todos los derechos reservados 2022. corporacionjess.com
             </small>
           </p>
         </b-col>
@@ -39,20 +39,23 @@
         </b-col>
         <b-col lg="6">
           <div class="d-flex">
-            <div class="mr-4">
+            <div class="mr-4 social">
               <BIconTwitter font-scale="2"/>
             </div>
-            <div class="mr-4">
+            <div class="mr-4 social">
               <BIconFacebook font-scale="2"/>
             </div>
-            <div class="mr-4">
+            <div class="mr-4 social">
               <BIconInstagram font-scale="2"/>
             </div>
           </div>
+          <br>
           <small>
-            Ubicación: Av. Soublette. Edf. Centro Soublette Piso 6. Ofc 6-C. Sector El Cardonal. La Guaira. Estado Vargas.<br>
-            Teléfonos: +58(212) 3319119 / +58(424) 2590227. <br>
-            E-mail: info@corporacionjess.com <br>
+            <div class="overlayFooter">
+              <span class="overlayFooter-text_primary">Ubicación:</span> Av. Soublette. Edf. Centro Soublette Piso 6. Ofc 6-C. Sector El Cardonal. La Guaira. Estado Vargas.<br>
+              <span class="overlayFooter-text_primary">Teléfonos:</span> +58(212) 3319119 / +58(424) 2590227. <br>
+              <span class="overlayFooter-text_primary">E-mail:</span> info@corporacionjess.com <br>
+            </div>
           </small>
         </b-col>
       </b-row>
@@ -76,5 +79,18 @@ export default {
   .footerLogo > img {
     padding-left: 15px;
     height: 45px;
+  }
+
+  .overlayFooter-text {
+    font-family: 'Nunito Sans', sans-serif;
+    font-weight: 400;
+    font-size: 60px;
+    &_primary {
+      color: #10CFC9;
+    }
+  }
+
+  .social {
+    color: #83C3E6;
   }
 </style>
